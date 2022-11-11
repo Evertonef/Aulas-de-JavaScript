948ddf656195efbaefc838b28426ca6db60e4519
Evertonef <evertonef@gmail.com>
2022-11-10 21:37:36 -0300

    Atualização

---
+++ Aula1.js
@@ -0,0 +1,10 @@
+// Variaveis e Operações
+
+let variavel = 10;  // eu posso mudar o valor do let.
+console.log(variavel);
+
+variavel = 20;
+console.log(variavel);
+
+const pi = 3.14;    // não pode mudar o valor de const
+console.log(pi);
